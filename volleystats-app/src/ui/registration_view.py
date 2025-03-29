f4ebc76394eac737f84dba60a0ac31e995d1c989
rautiais <rautiainen.e.salla@gmail.com>
2025-03-29 23:01:37 +0200

    Starting to work on registration view

--- volleystats-app/src/ui/registration_view.py
+++ volleystats-app/src/ui/registration_view.py
@@ -1,3 +1,27 @@
+from tkinter import ttk, constants, StringVar
+from services.user_service import UserService
+
 class RegisterView:
-    def __init__(self):
-        pass
+    def __init__(self, root, handle_login_view):
+        self.root = root
+        self.handle_login_view = handle_login_view
+        self.user_service = UserService()
+        self.frame = None
+        self.username_entry = None
+        self.password_entry = None
+    
+    def pack(self):
+        self.frame(fill=constants.X)
+
+    def destroy(self):
+        self.frame.destroy()
+
+    def register_handler(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if self.user_service.create_user(username, password):
+            self.handle_login_view()
+        else:
+            pass
+
