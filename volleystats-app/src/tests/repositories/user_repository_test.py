d60bf48eff9b68b668b99c388e8ef09b753f70e1
rautiais <rautiainen.e.salla@gmail.com>
2025-03-31 15:22:26 +0300

    Made tests for registration

---
+++ volleystats-app/src/tests/repositories/user_repository_test.py
@@ -0,0 +1,31 @@
+import unittest
+from repositories.user_repository import UserRepository
+from entities.user import User
+
+class TestUserRepository(unittest.TestCase):
+    def setUp(self):
+        self.user_repository = UserRepository()
+
+    def test_create_user(self):
+        """Test that user can be created"""
+        user = self.user_repository.create_user("Paavo", "Kissa123")
+        self.assertEqual(user.username, "Paavo")
+        self.assertEqual(user.password, "Kissa123")
+
+    def test_find_by_username_existing_user(self):
+        """Test that existing user can be found"""
+        self.user_repository.create_user("Paavo", "Kissa123")
+        user = self.user_repository.find_by_username("Paavo")
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, "Paavo")
+
+    def test_find_by_username_nonexistent_user(self):
+        """Test that nonexistent user returns None"""
+        user = self.user_repository.find_by_username("Sirpa")
+        self.assertIsNone(user)
+
+    def test_create_duplicate_user(self):
+        """Test that a used username cannot be created"""
+        self.user_repository.create_user("Paavo", "Kissa123")
+        result = self.user_repository.create_user("Paavo", "Koira123")
+        self.assertIsNone(result)
--- volleystats-app/src/tests/services/user_service_test.py
+++ volleystats-app/src/tests/services/user_service_test.py
@@ -5,6 +5,23 @@ class TestUserService(unittest.TestCase):
     def setUp(self):
         self.user_service = UserService()
 
-    def test_create_user_valid_credentials(self):
+    def test_create_user_with_valid_credentials(self):
+        """Test that creating user with valid credentials works"""
         result = self.user_service.create_user("Paavo", "Kissa123")
         self.assertTrue(result)
+
+    def test_create_user_with_too_short_username(self):
+        """Test that creating user with too short username doesn't work"""
+        result = self.user_service.create_user("Pa", "Kissa123")
+        self.assertFalse(result)
+
+    def test_create_user_with_too_short_password(self):
+        """Test that creating user with too short password doesn't work"""
+        result = self.user_service.create_user("Paavo", "Kissa")
+        self.assertFalse(result)
+
+    def test_create_duplicate_user(self):
+        """Test that a used username cannot be created"""
+        self.user_service.create_user("Paavo", "Kissa123")
+        result = self.user_service.create_user("Paavo", "Koira123")
+        self.assertFalse(result)
