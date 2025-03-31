2803165de4d845a16a15679e9f76d3717488c199
rautiais <rautiainen.e.salla@gmail.com>
2025-03-31 15:47:39 +0300

    Deleted unnecessary task

--- volleystats-app/tasks.py
+++ volleystats-app/tasks.py
@@ -7,10 +7,6 @@ def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
 @task
-def build(ctx):
-    ctx.run("python3 src/build.py", pty=True)
-
-@task
 def test(ctx):
     ctx.run("pytest", pty=True)
 
