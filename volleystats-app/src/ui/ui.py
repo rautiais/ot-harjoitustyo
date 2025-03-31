b8c41c237e1a4ad0af144865d33ce6a5abee9df9
rautiais <rautiainen.e.salla@gmail.com>
2025-03-31 13:13:48 +0300

    Moved the ui class to its own file. Added comments

---
+++ volleystats-app/src/ui/ui.py
@@ -0,0 +1,49 @@
+from tkinter import Tk
+from ui.login_view import LoginView
+from ui.registration_view import RegistrationView
+
+
+class UI:
+    """Class for user interface"""
+
+    def __init__(self, root):
+        """Entity is created to handle all ui functionality. Current view keeps track of which view is displayed currently"""
+        self._root = root
+        self._current_view = None
+
+    def start(self):
+        """Shows the login view when the application starts"""
+        self._show_login_view()
+
+    def _hide_current_view(self):
+        """Hides current window if it exists"""
+        if self._current_view:
+            self._current_view.destroy()
+
+    def _handle_register(self):
+        """Callback method for switching to registration view"""
+        self._show_register_view()
+
+    def _handle_login(self):
+        """Callback method for switching back to login view"""
+        self._show_login_view()
+
+    def _show_login_view(self):
+        """Hides the current view"""
+        self._hide_current_view()
+        """Creates new login view and passes the registration handle"""
+        self._current_view = LoginView(
+            self._root,
+            self._handle_register
+        )
+        self._current_view.pack()
+
+    def _show_register_view(self):
+        """Hides the current view"""
+        self._hide_current_view()
+        """Creates new register view and passes the login handler"""
+        self._current_view = RegistrationView(
+            self._root,
+            self._handle_login
+        )
+        self._current_view.pack()
